f60cff0f1ae8570eae2447eea0267f14959a553b
VISHVA E <vishvamgr27@gmail.com>
2025-12-05 18:59:26 +0530

    Delete BackEnd/WheelWiseB/WheelWise directory

--- BackEnd/WheelWiseB/WheelWise/src/main/java/com/example/WheelWise/repositories/BookingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.WheelWise.repositories;
-
-import com.example.WheelWise.entities.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
